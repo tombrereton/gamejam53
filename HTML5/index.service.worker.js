77b7096776bc91947557e327b1e3184152f944a2
tombrereton <tombrereton@users.noreply.github.com>
2023-01-16 06:28:02 +0000

    Deploying to gh-pages from @ tombrereton/gamejam53@ac3bd68d2ec2e24857ff32b7da65f6853a06f5b9 🚀

--- HTML5/index.service.worker.js
+++ HTML5/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1673849598|2102810";
+const CACHE_VERSION = "1673850477|2057094";
 const CACHE_PREFIX = "gamejam53-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
